d57d99bbfd614a4bb261eeaac8bbc2be66b3c625
ryan2625 <ryanfreas19@augustana.edu>
2023-11-29 15:54:53 -0600

    Adding scss styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey</h1>
+      <button>Clock me</button>
+      <h2>WHAT</h2>
+      <button>CLICK ME AGAION</button>
     </div>
   );
 }
