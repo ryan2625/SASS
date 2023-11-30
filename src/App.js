19e946b2b4efe6993f2ae4c23facee23a36a13eb
ryan2625 <ryanfreas19@augustana.edu>
2023-11-29 23:24:00 -0600

    Experimenting with MIXINS

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
-      <button>Clock me</button>
-      <h2>WHAT</h2>
-      <button>CLICK ME AGAION</button>
-      <button>asds</button>
+      <button>Hello World</button>
+      <h1>HEY WHAT THE </h1>
     </div>
   );
 }
